e6d60eb9d3eb72695a329a447c9dd910e229bd53
Norze <123885505+N0rz3@users.noreply.github.com>
2023-06-04 21:32:43 +0200

    Add files via upload

---
+++ core/lib/check_url.py
@@ -0,0 +1,60 @@
+import requests
+
+def check(url) -> bool:
+        """
+        Check status of requests 
+
+
+        Parameters:
+            url (str): url that needs to be checked 
+
+        Returns:
+            bool: True if the responds of the request is good else False
+        """
+
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return True
+            
+
+            else:
+                return False
+
+        except requests.exceptions.RequestException:
+            return False
+    
+
+def guette_moi_ça(url):
+    
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return "200 ✔️ "
+    
+    elif response.status_code == 404:
+        return "404 ❌"
+     
+    elif response.status_code == 403:
+        return "403 ❌"
+    
+    elif response.status_code == 301:
+        return "301 ❌"
+    
+    elif response.status_code == 302:
+        return "302 ❌"
+    
+    elif response.status_code == 500:
+        return "500 ❌"
+    
+    elif response.status_code == 502:
+        return "502 ❌"
+    
+    elif response.status_code == 503:
+        return "503 ❌"
+    
+    elif response.status_code == 504:
+        return "504 ❌"
+
+    elif response.status_code == 401:
+        return "401 ❌"
---
+++ core/namesint.py
@@ -0,0 +1,56 @@
+import utils
+import argparse
+
+from modules import positives, search, version
+
+
+if __name__ == "__main__":
+    opt = f"""
+
+                                      {utils.RED}_____{utils.WHITE}       {utils.RED}_____{utils.WHITE}
+{utils.RED}_____________ _______ __________________{utils.WHITE}(_){utils.RED}________  {utils.WHITE}/_
+{utils.RED}__{utils.WHITE}  __ \  __ `/_  __ `__ \  _ \_  ___/_  /__  __ \  __/
+{utils.RED}_{utils.WHITE}  / / / /_/ /_  / / / / /  __/(__  )_  / _  / / / /_  
+/_/ /_/\__,_/ /_/ /_/ /_/\___//____/ /_/  /_/ /_/\__/  """
+                                                                                    
+    text = """
+            using OSINT✨
+        https://github.com/N0rz3
+
+
+Usage: python namesint.py [-h] {--search, --positives} ...
+
+Options:
+-h, --help                      show this help message and exit
+-s, --search                    Find all target accounts
+-p, --positives                 Show positives accounts
+-v, --version                   Displays the version of the tool
+"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search", help="Find all target accounts")
+    parser.add_argument("-p", "--positives", help="Show positives accounts")
+    parser.add_argument("-v", "--version", help="Displays the version of the tool", action="store_true")
+    args = parser.parse_args()
+
+    if args.search:
+        print(utils.banner)
+        user = args.search
+        search.User.check(user)
+        exit()
+
+    elif args.positives:
+        print(utils.banner)
+        only = args.positives
+        positives.Po.check(only)
+        exit()
+
+
+    elif args.version:
+        print(version.ver())
+        exit()
+
+    else:
+        print(opt)
+        print(text)
+        exit()
