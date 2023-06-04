8bca82bd8b77eea5156dd5a3b00600784509fe08
Norze <123885505+N0rz3@users.noreply.github.com>
2023-06-04 19:59:21 +0200

    Update only_find.py

--- core/modules/only_find.py
+++ core/modules/only_find.py
@@ -30,7 +30,7 @@ class On:
                 if check_url.check(url):
                     fake += 1
                     numbr += 1
-                    print(f"[{utils.GREEN}!{utils.WHITE}] - {utils.BLUE}{fake}{utils.WHITE} - {utils.GREEN}{entry['site_name']}{utils.WHITE} ({utils.YELLOW}{url}{utils.WHITE}) [{check_url.gette_moi_ça(url)}]")
+                    print(f"[{utils.GREEN}!{utils.WHITE}] - {utils.BLUE}{fake}{utils.WHITE} - {utils.GREEN}{entry['site_name']}{utils.WHITE} ({utils.YELLOW}{url}{utils.WHITE}) [{check_url.guette_moi_ça(url)}]")
 
             print(f"\n[{utils.GREEN}:){utils.WHITE}] Finish ! ({utils.YELLOW}{numbr} results{utils.WHITE}) Time : ({utils.YELLOW}{round(time.time() - start, 1)}s{utils.WHITE})")
 
