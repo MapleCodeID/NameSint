3c50f051bdfae377a92fbb0f02619506fd55440d
Norze <123885505+N0rz3@users.noreply.github.com>
2023-06-04 19:48:36 +0200

    Update only_find.py

--- core/modules/only_find.py
+++ core/modules/only_find.py
@@ -21,14 +21,16 @@ class On:
             print(f"[{utils.YELLOW}🕵️‍♂️{utils.WHITE}] Target name : {utils.YELLOW}{username}{utils.WHITE}\n")
 
             numbr = 0
+            fake = 0
             start = time.time()
 
             for entry in data:
                 url = entry['url'].replace('username', username)
 
                 if check_url.check(url):
+                    fake += 1
                     numbr += 1
-                    print(f"[{utils.GREEN}!{utils.WHITE}] {entry['site_name']} ({utils.YELLOW}{url}{utils.WHITE}) [{check_url.gette_moi_ça(url)}]")
+                    print(f"[{utils.GREEN}!{utils.WHITE}] - {utils.BLUE}{fake}{utils.WHITE} - {utils.GREEN}{entry['site_name']}{utils.WHITE} ({utils.YELLOW}{url}{utils.WHITE}) [{check_url.gette_moi_ça(url)}]")
 
             print(f"\n[{utils.GREEN}:){utils.WHITE}] Finish ! ({utils.YELLOW}{numbr} results{utils.WHITE}) Time : ({utils.YELLOW}{round(time.time() - start, 1)}s{utils.WHITE})")
 
